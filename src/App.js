50b37a86c1deea8b67d8cd61597500de65e04404
YAM GAMING <yamgaming@YAMs-MacBook-Pro.local>
2021-08-14 18:49:03 -0400

    remove elements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-
+import {useState} from "react";
 import Todo from "./components/Todo"
 function App() {
+
+    const data = [
+      {
+ 
+        title:"Learn Other Languaje"
+      },
+      {
+
+        title:"Learn ReactJs"
+      },
+      {
+
+        title:"Create a Api"
+      },
+      {
+   
+        title:"Be a good programer"
+      },
+    ];
+
+    const [todoData,setTodoData] = useState(data);
+
+    const delectElment = (e) => {
+        let tmData = todoData;
+            tmData.splice(e,1);
+            setTodoData(tmData);
+    }
+
+
   return (
     <div className="App">
       <h1>My Todo</h1>
-       <Todo title="Learn other languaje" />
-       <Todo title="Learn ReactJs" />
-       <Todo title="Create a Api" />
-       <Todo title="Be a good programer" />
+      {
+        todoData.map((e, k )=> <Todo onDelect={delectElment} key={k + '-data'} index={k} title={e.title} />)
+      }
+      
+      <button className="btn btn-add-new"> + </button>
    
     </div>
   );
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,9 +5,11 @@ import Backdrop from "./Backdrop"
 
 function Todo(props)
 {   
-    
+
+
     const [modalOpen, setModalOpen] = useState(false);
-    const {title} = props;
+    const [itemTrue, setItemTrue] = useState(true);
+    const {index,title} = props;
 
     function delectHandler(){
         setModalOpen(true);
@@ -15,16 +17,24 @@ function Todo(props)
 
     function closeModalHaldlers(){
         setModalOpen(false);
+
     }
-    return(<div className="card"> 
+    function confirmlHaldlers(){
+        setModalOpen(false);
+        setItemTrue(false);
+        props.onDelect(index);
+
+
+    }
+    return(<div>{itemTrue ? <div className="card"> 
     <h2>{title}</h2>
     <div className="actions">
         <p>description, some thin to do...</p>
       <button className="btn" onClick={delectHandler} >Delecte</button>
     </div>
-    {modalOpen ? <Modal  onCancel={closeModalHaldlers}  onConfirm={closeModalHaldlers}   /> : null }
+    {modalOpen ? <Modal  onCancel={closeModalHaldlers}  onConfirm={confirmlHaldlers}   /> : null }
     {modalOpen ? <Backdrop onCancel={closeModalHaldlers}  /> : null }
-  </div>);
+  </div> : null } </div>);
 }
 
 export default Todo;
